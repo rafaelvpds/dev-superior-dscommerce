60e0e165e5b345fe0710a64d379e0b25060d3cb5
Rafael <rafaelvpds.dev09@gmail.com>
2023-12-21 19:52:53 -0300

    Relacionamento muitos-para-muitos

---
+++ src/main/java/com/example/devsuperior/dsecomecer/entities/Category.java
@@ -0,0 +1,45 @@
+package com.example.devsuperior.dsecomecer.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+}
