c2570dfbc08ef6cb607f4e7110456521e2e7d01f
Rafael <rafaelvpds.dev09@gmail.com>
2024-01-01 11:28:14 -0300

    GET product paged

--- src/main/java/com/example/devsuperior/dsecomecer/Service/ProductService.java
+++ src/main/java/com/example/devsuperior/dsecomecer/Service/ProductService.java
@@ -1,6 +1,9 @@
 package com.example.devsuperior.dsecomecer.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +24,17 @@ public class ProductService {
         // return productDTO;
         Product product = productRepository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        // Optional<Product> result = productRepository.findById(id);
+        // Product product = result.get();
+        // ProductDTO productDTO = new ProductDTO(product);
+        // return productDTO;
+        PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
+        Page<Product> productList = productRepository.findAll(pageable);
+        return productList.map(x -> new ProductDTO(x));
     }
 
 }
--- src/main/java/com/example/devsuperior/dsecomecer/controller/ProductController.java
+++ src/main/java/com/example/devsuperior/dsecomecer/controller/ProductController.java
@@ -1,6 +1,11 @@
 package com.example.devsuperior.dsecomecer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +24,14 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id) {
 
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(
+            @PageableDefault(page = 0, size = 12) @SortDefault.SortDefaults({
+                    @SortDefault(sort = "name", direction = Sort.Direction.ASC)
+            }) Pageable pageable) {
+
+        return service.findAll(pageable);
     }
 }
